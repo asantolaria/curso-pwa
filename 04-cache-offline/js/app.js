1e7a48d2b14c16bb96235513eab1d5775c357a0c
alexsantolaria <87411578+alexsantolaria@users.noreply.github.com>
2022-03-18 11:37:41 +0100

    guardar en cache archivos

--- 04-cache-offline/js/app.js
+++ 04-cache-offline/js/app.js
@@ -3,30 +3,30 @@
 if ( navigator.serviceWorker ) {
     navigator.serviceWorker.register('/sw.js');
 }
-
-if (window.caches) {
-    caches.open('prueba-1'); // abre, si no existe la crea
-    caches.open('prueba-2'); // abre, si no existe la crea
-    // caches.has('prueba-3').then(console.log(existe)); // pregunta si existe
-    // caches.delete('prueba-1').then(console.log); // borrar cache
-    caches.open('cache-v1.1').then(cache => {
-       cache.add('/index.html');
-       cache.addAll([
-           '/index.html',
-           '/css/style.css',
-           '/img/main.jpg',
-       ]).then(() => {
-           // cache.delete('/css/style.css');
-           cache.put('index.html', new Response('Hola Mundo'));
-       });
-
-       // leer archivo y mostrarlo en consola
-       // cache.match('/index.html').then(res => {
-       //     res.text().then(console.log);
-       // });
-
-    });
-
-    caches.keys().then(console.log);
-
-}
+//
+// if (window.caches) {
+//     caches.open('prueba-1'); // abre, si no existe la crea
+//     caches.open('prueba-2'); // abre, si no existe la crea
+//     // caches.has('prueba-3').then(console.log(existe)); // pregunta si existe
+//     // caches.delete('prueba-1').then(console.log); // borrar cache
+//     caches.open('cache-v1.1').then(cache => {
+//        cache.add('/index.html');
+//        cache.addAll([
+//            '/index.html',
+//            '/css/style.css',
+//            '/img/main.jpg',
+//        ]).then(() => {
+//            // cache.delete('/css/style.css');
+//            cache.put('index.html', new Response('Hola Mundo'));
+//        });
+//
+//        // leer archivo y mostrarlo en consola
+//        // cache.match('/index.html').then(res => {
+//        //     res.text().then(console.log);
+//        // });
+//
+//     });
+//
+//     caches.keys().then(console.log);
+//
+// }
--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,31 +1,22 @@
-self.addEventListener('fetch', event => {
+self.addEventListener('install', e => {
+   const cacheProm = caches.open('cache-1')
+       .then(cache => {
+           return cache.addAll([
+               '/index.html',
+               '/css/style.css',
+               '/img/main.jpg',
+               'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+               '/js/app.js'
+           ]);
+       });
 
-    // const offlineResp = new Response(`
-    //     <!DOCTYPE html>
-    //     <html lang="en">
-    //     <head>
-    //         <meta charset="UTF-8">
-    //         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    //         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    //         <title>Mi PWA</title>
-    //     </head>
-    //     <body class="container p-3">
-    //         <h1>Offline Mode</h1>
-    //     </body>
-    //     </html>
-    // `,  {
-    //     headers: {
-    //         'Content-Type':'text/html'
-    //     }
-    // });
-
-    const offlineResp = fetch('pages/offline.html');
-
-    const resp = fetch(event.request)
-                    .catch(() => offlineResp );
-
-    // Es el SW quien hace la peticion
-    event.respondWith(resp);
-})
+   // cacheProm tiene devolver la promesa para que funcione el waitUntil. Por eso hay que poner el return del cache.addAll()
+   e.waitUntil(cacheProm);
+});
 
 
+self.addEventListener('fetch', e => {
+
+
+
+});
