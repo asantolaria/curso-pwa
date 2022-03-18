96cee0d0c7eaeb80d7f860f7bfe73df59426d11a
alexsantolaria <87411578+alexsantolaria@users.noreply.github.com>
2022-03-18 12:01:00 +0100

    cache with network fallback

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,7 +1,9 @@
+const CACHE_NAME = 'cache-1';
 self.addEventListener('install', e => {
-   const cacheProm = caches.open('cache-1')
+   const cacheProm = caches.open(CACHE_NAME)
        .then(cache => {
            return cache.addAll([
+               '/',
                '/index.html',
                '/css/style.css',
                '/img/main.jpg',
@@ -16,7 +18,27 @@ self.addEventListener('install', e => {
 
 
 self.addEventListener('fetch', e => {
+    // 1- Cache Onlye: que la aplicación siempre cargue desde el cache
+    // e.respondWith( caches.match( e.request ) );
 
+    // 2- Cache  with Network Fallback: intenta leer cache y si no carga de la red
+    const respuesta = caches.match( e.request )
+        .then( res => {
+            if (res) return res;
+            // No existe el archivo
+            // Tengo que ir a la web
+            console.log('No existe', e.request.url);
+            return fetch( e.request )
+                .then( newResponse => {
+                    caches.open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(e.request, newResponse)
+                        })
+                    return newResponse.clone();
+                });
 
+        });
+
+    e.respondWith(respuesta);
 
 });
