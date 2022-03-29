ccf05f81534aaf800eec8cc131b32987fbbe02e7
Alex Santolaria <alex.santolaria@aeinnova.com>
2022-03-29 09:53:57 +0200

    app angular con SW funcionando

--- 13-paises/src/app/app.module.ts
+++ 13-paises/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { PaisComponent } from './pages/pais/pais.component';
 import { PaisesComponent } from './pages/paises/paises.component';
 import {AppRoutingModule} from "./app-routing.module";
 import { HttpClientModule} from "@angular/common/http";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,12 @@ import { HttpClientModule} from "@angular/common/http";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
